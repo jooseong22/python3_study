cfbbdf61694d5241c95b2ece9fb5d84ea9ef0a31
jooseong22 <jooseong3@gmail.com>
2025-01-26 10:10:51 +0900

    Create print1_3.py
    출력문, format변수 삽입

---
+++ print1_3.py
@@ -0,0 +1,47 @@
+print("a : {}".format(12))
+print("{} {} {}".format(1, 2, 3))
+print("안녕하세요 저는 {}이고 나이는 {}입니다.".format("홍길동", 20))
+name = "김철수"
+age = 30
+print("이름 : {}, 나이 : {}".format(name, age))
+
+a = 10
+b = 5
+print("a : {}".format(b))
+print("b : {}".format(a))
+
+
+a = 10 + 10 # a라는 변수에 10 + 10 의 결과값을 대입한다.
+print(a)
+
+print(10 + 10)
+
+
+# 연산자 종류
+
+print(10 / 3)
+print(10 % 3)
+
+a = 10
+print(a > 10)
+print(a >= 10)
+
+print("T and T : {}".format(a > 9 and a >= 10))
+print("F and T : {}".format(a > 10 and a >= 10))
+print("T and F : {}".format(a >= 10 and a > 10))
+print("F and F : {}".format(a > 10 and a >= 11))
+print("T or T : {}".format(a > 9 or a >= 10))
+print("F or T : {}".format(a > 10 or a >= 10))
+print("T or F : {}".format(a >= 10 or a > 10))
+print("F or F : {}".format(a > 10 or a >= 11))
+
+
+print("a == 10 : {}".format(a == 10))
+print("a != 10 : {}".format(a != 10))
+
+
+이름 = "홍길동"
+print(이름)
+
+Age = 10
+print(Age)
